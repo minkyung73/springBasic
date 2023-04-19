3d7307e593949aa53e270688a6dd9cf90e4b167d
minkyung <minkyung7365@gmail.com>
2023-04-20 02:01:04 +0900

    static, mvc, api

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -8,22 +8,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-    @GetMapping("hello")
+    @GetMapping("hello")     // '주소/hello'로 들어오면 이 코드랑 mapping 해줌
     public String hello(Model model) {
         model.addAttribute("data", "spring!!");
-        return "hello";
+        return "hello"; // return 값인 hello.html을 찾음
     }
 
+    // ex) http://localhost:8080/hello-mvc?name=stst
+    // RequestParam으로 값을 넣어주지 않았을 때
+    // 에러 발생: Required request parameter 'name' for method parameter type String is not present
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
-        return "hello-template";
+        return "hello-template";    // hello-template.html을 찾는다.
     }
 
     @GetMapping("hello-string")
-    @ResponseBody
+    @ResponseBody   // http의 body부에 이 데이터를 직접 넣어주겠다~ 라느 뜻
+                    // name에 spring이라고 하면,
+                    // 이 문자가 그대로 감 (html 없이)
     public String helloString(@RequestParam("name") String name) {
-        return "hello " + name;
+        return "hello " + name; // html tag가 하나도 없이 return 하는 문자 그대로 들어감
     }
 
     @GetMapping("hello-api")
@@ -31,7 +36,7 @@ public class HelloController {
     public Hello helloApi(@RequestParam("name") String name) {
         Hello hello = new Hello();
         hello.setName(name);
-        return hello;
+        return hello;   // 객체 반환 (api의 특징)
     }
 
     static class Hello {
